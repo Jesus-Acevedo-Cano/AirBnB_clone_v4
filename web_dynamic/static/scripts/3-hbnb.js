e992b07c450b6f28474a1fc97e611298cb977779
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-05-16 23:08:10 +0000

    advances

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+  const amenities = [];
+  $('input[type=checkbox]').click(function () { // Select all checkbox elements
+    if (this.checked) {
+      amenities.push($(this).attr('data-name'));
+      // Other way: amenities.push($(this).data('name'))
+    } else {
+      const index = amenities.indexOf($(this).attr('data-name'));
+      amenities.splice(index, 1); // Delete element by index
+    }
+    // Now, let's add list elements to h4 within amenities css class
+    if (amenities.length > 0) {
+      $('.amenities > h4').text(amenities.join(', '));
+    } else {
+      /* If there is just one element, and I des-select it,
+      it'll stay in h4, so... we need to replace it with &nbsp */
+      $('.amenities > h4').html('&nbsp');
+    }
+  });
+  $.get("http://0.0.0.0:5001/api/v1/status/", function(data, status) {
+    if (data.status === 'OK') {
+      $("DIV#api_status").addClass('available');
+    } else {
+      $("DIV#api_status").removeClass('available');
+    }
+  });
+  $.get("http://0.0.0.0:5001/api/v1/places_search/", function(data, status) {
+    
+  });
+});
