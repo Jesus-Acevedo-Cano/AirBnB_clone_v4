1eb0c432393ab031408bc939c728f03b400fd82b
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-05-18 06:15:24 +0000

    hope it works(scripts 3hbnb and 4hbnb)

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -42,4 +42,26 @@ $(document).ready(function () {
       }
     }
   });
+
+  $('button').click(function () {
+    $.ajax({
+      type: 'POST',
+      url: 'http://0.0.0.0:5001/api/v1/places_search',
+      contentType: 'application/json',
+      dataType: 'json',
+      data: JSON.stringify({ amenities: Object.keys(amenities) }),
+      success: function (data) {
+        $('article').remove();
+        for (const places of data) {
+          $('section.places').append('<article><h2>' +
+            places.name + '</h2><div class="price_by_night"><p>$' +
+              places.price_by_night + '</p></div><div class="information"><div class="max_guest"><div class="guest_image"></div><p>' +
+                places.max_guest + '</p></div><div class="number_rooms"><div class="bed_image"></div><p>' +
+                  places.number_rooms + '</p></div><div class="number_bathrooms"><div class="bath_image"></div><p>' +
+                    places.number_bathrooms + '</p></div></div><div class="description"><p>' +
+                      places.description + '</p></div></article>');
+        }
+      }
+    });
+  });
 });
